1fbf4bf13c78909e957b20224fbabf2f2c05718e
pedroribeirodev <pedro.bonfim.13@gmail.com>
2019-10-29 21:06:40 -0300

    New - Removendo as classes, e utilizando react hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,14 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
-    'no-console': ["error", { allow: ['tron']}]
+    'no-console': ["error", { allow: ['tron']}],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
